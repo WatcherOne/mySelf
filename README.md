c8f4543d69bf7dd8577ec0401747fe6ceb9111ba
朱波 <zhubo@terminus.io>
2017-07-17 23:21:46 +0800

    fixed router route

--- README.md
+++ README.md
@@ -16,6 +16,10 @@
 ### 常见问题
   1. 执行 npm run server 时，报错webpack-dev-server command not found <br>
      需要全局安装 npm install webpack-dev-server -g
+  2. <Router> 里面有且只有一个child
+  3. <Route > 必须有component
+  4. <Route></Route> 嵌套时也是嵌套着组件的结构
+  5. <Route> 4.0 后的嵌套好像有不同，还是大问题，后续讨论
 
 # study
     不引用路由时
@@ -45,4 +49,29 @@
     <!-- 取得页面 hash 字段，然后传入 react 组件作为 route 参数  -->
     const route = window.location.hash.substr(1);  //注意的是：网页url是以（#开头的）8080/#/home
     ReactDOM.render(<App route={route} />, document.getElementById('app'))
+  ```
+
+      引用路由时
+  ```javascript
+    <IndexRoute component={Home} />
+    // 默认情况加载的子组件 之前版本是 DefaultRoute
+    <Route />
+    // 用于声明式地映射路由规则到你多层嵌套的应用组件
+    // 属性name(optional)
+    // name 在路由中是唯一的，被使用在 Link 组件和路由转换的方法中。
+    // 属性path(optional)
+    // 在url中使用的路径，如果不填写的话，路径就是name，如果name也没有的话，默认就是 /.
+    // children, 可嵌套使用
+    <Redirect from="/" to="/index" />
+    // 组件用于路由的跳转，即用户访问一个路由，会自动跳转到另一个路由。
+    <Route path="/" component={App}>
+      ＜IndexRedirect to="/index" />
+    </Route>
+    // IndexRedirect组件用于访问根路由的时候，将用户重定向到某个子组件。
+    <Router history={browserHistory}>
+      <Route />
+    </Router>
+    // Router组件本身只是一个容器，真正的路由要通过Route组件定义。而且是有一个child element！！！
+    // 组件有一个参数history，它的值browserHistory表示：路由的切换由URL的hash变化决定，即URL的#部分发生变化。
+    // 举例来说，用户访问http://www.example.com/，实际会看到的是http://www.example.com/#/  跟 无路由时对应上
   ```
--- app/index.js
+++ app/index.js
@@ -1,39 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Route, browserHistory, BrowserRouter as Router, IndexRoute } from 'react-router-dom'
+import { Route, IndexRoute, Redirect, browserHistory, BrowserRouter as Router } from 'react-router-dom'
 import 'antd/dist/antd.css'
+import Outer from './outer'
 import Home from './home'
 import UserCenter from './userCenter'
 
-// const AppRouter = () => {
-//   return (
-//     <Router history={browserHistory}>
-//       <div>
-//         <Route exact path='/' component={Home} />
-//         <Route path='/index' component={Home} />
-//         <Route path='/user/center' component={UserCenter} />
-//       </div>
-//     </Router>
-//   )
-// }
+// 引用路由
+/* 为什么上下创建react组件的方式不一样，就回到创建react组件方式分类。
+ * 该创建方式属于只用作展示，无法控制组件的渲染，下面的是es5方式写的。
+ */
+const App = () => {
+  return (
+    <Router history={browserHistory}>
+      <div>
+        <Route path='/' component={Home} />
+        <Route path='/index' component={Home} />
+        <Route path='/user/center' component={UserCenter} />
+      </div>
+    </Router>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'))
 
 // 不引用路由的react
-const App = React.createClass({
-  render () {
-    let Child = null
-    const route = this.props.route
-    switch(route) {
-      case '/home': Child = Home; break;
-      case '/user/center': Child = UserCenter; break;
-      default: Child = Home;
-    }
-    return (
-      <div>
-        <Child/>
-      </div>
-    )
-  }
-})
+// const App = React.createClass({
+//   render () {
+//     let Child = null
+//     const route = this.props.route
+//     switch(route) {
+//       case '/home': Child = Home; break;
+//       case '/user/center': Child = UserCenter; break;
+//       default: Child = Home;
+//     }
+//     return (
+//       <div>
+//         <Child/>
+//       </div>
+//     )
+//   }
+// })
 
-const route = window.location.hash.substr(1);
-ReactDOM.render(<App route={route} />, document.getElementById('app'))
+// const route = window.location.hash.substr(1);
+// ReactDOM.render(<App route={route} />, document.getElementById('app'))
